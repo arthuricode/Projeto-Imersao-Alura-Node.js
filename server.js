6b1b5d4e32e9ab3b60f0aed77b650434b9af9071
Arthuricode <arthurcallima@gmail.com>
2024-11-21 15:59:55 -0300

    second commit

--- server.js
+++ server.js
@@ -1,27 +1,9 @@
 import express from 'express';
-
-const posts = [
-    { id: 1, descricao: "Gato dormindo", imagem: "https://placecats.com/millie/300/150" },
-    { id: 2, descricao: "Gato jogando bola", imagem: "https://placecats.com/millie/300/150" },
-    { id: 3, descricao: "Gato socando uma árvore", imagem: "https://placecats.com/millie/300/150" },
-  ];
+import routes from './src/routes/postsRoutes.js';
 
 const app = express();
+routes(app)
+
 app.listen(3000, () => {
     console.log("Servidor escutando...");
-});
-
-app.get("/posts", (req, res) => {
-    res.status(200).json(posts);
-});
-
-function buscarPostPorID(id){
-    return posts.findIndex((post) => {
-        return post.id === Number(id)
-    })
-}
-
-app.get("/posts/:id", (req, res) => {
-    const index = buscarPostPorID(req.params.id)
-    res.status(200).json(posts[index]);
 });
