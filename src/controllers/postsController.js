4f78ff725ad84cf8c653b57fea52d277e9cfe7eb
Arthuricode <arthurcallima@gmail.com>
2024-11-25 16:23:08 -0300

    fourth commit

--- src/controllers/postsController.js
+++ src/controllers/postsController.js
@@ -1,5 +1,6 @@
-import {getTodosPosts, criarPost} from "../models/postsModel.js";
+import {getTodosPosts, criarPost, atualizarPost} from "../models/postsModel.js";
 import fs from "fs";
+import gerarDescricaoComGemini from "../services/geminiService.js";
 
 export async function listarPosts(req, res){
     const posts = await getTodosPosts();
@@ -30,6 +31,25 @@ export async function uploadImagem(req, res) {
         fs.renameSync(req.file.path, imagemAtualizada);
         res.status(200).json(postCriado);  
     } catch(erro) {
+        console.error(erro.message);
+        res.status(500).json({"Erro":"Falha na requisição"})
+    }
+}
+
+export async function atualizarNovoPost(req, res) {
+    const id = req.params.id;
+    const urlImagem = `https://localhost:3000/${id}.png`   
+    try{
+        const imgBuffer = fs.readFileSync(`uploads/${id}.png`)
+        const descricao = await gerarDescricaoComGemini(imgBuffer)
+        const post = {
+            imgUrl: urlImagem,
+            descricao: descricao,
+            alt: req.body.alt
+        }
+        const postCriado = await atualizarPost(id, post);
+        res.status(200).json(postCriado);   
+    }catch(erro){
         console.error(erro.message);
         res.status(500).json({"Erro":"Falha na requisição"})
     }
