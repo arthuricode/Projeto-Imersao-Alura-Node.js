18ca679402a7b5d1c1b4a79c672220d483e22f57
Arthuricode <arthurcallima@gmail.com>
2024-11-25 19:53:22 -0300

    fourth commit

--- src/services/geminiService.js
+++ src/services/geminiService.js
@@ -5,7 +5,7 @@ const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
 export default async function gerarDescricaoComGemini(imageBuffer) {
   const prompt =
-    "Gere uma descrição em português do Brasil para a seguinte imagem";
+    "Gere uma descrição resumida em português do Brasil para a seguinte imagem";
 
   try {
     const image = {
